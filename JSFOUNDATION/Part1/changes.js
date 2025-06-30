f695623cc5225150f012a7694592378a206c1d5d
Srv228 <deysourav756@gmail.com>
2025-06-30 12:21:49 +0530

    first upload

---
+++ JSFOUNDATION/Part1/changes.js
@@ -0,0 +1,15 @@
+let gameName = "spiderman";
+
+gameName = "GTA6";
+
+
+console.log(gameName);
+
+const  userName = "Srv228"
+
+userName = "Srv_228"
+
+console.log(userName);
+
+// /TypeError: Assignment to constant variable.
+
---
+++ JSFOUNDATION/Part1/datatype.js
@@ -0,0 +1,34 @@
+console.log("Hello");
+// console.log("chai");
+/*
+String
+Number
+Boolean
+BigInt
+
+Undefined
+null
+
+Object
+
+
+Symbol
+
+
+*/
+
+// var score = 102;
+
+let score = 102;
+let name = "Sourav Dey";
+let isLoggedIn = false;
+
+// Object
+
+let teaTypes = ["lemon tea", "orange tea", "oolong tea"];
+let user = { firstname: "sourav", lastname: "dey" };
+
+let getScore = score;
+
+console.log(getScore);
+
