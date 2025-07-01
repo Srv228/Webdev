411aec47e791dbb0fdbdeac2c630129d65bdb634
Srv228 <deysourav756@gmail.com>
2025-07-01 11:33:22 +0530

    Added new files

---
+++ JSFOUNDATION/Part1/logical.js
@@ -0,0 +1,16 @@
+//  && --> AND == All cond true
+//  || -->  OR == 1 true
+//  ! --> reverse
+
+let isLoggedin = true;
+let isPaid = false;
+
+// console.log(isLoggedin && isPaid);
+
+let isEmailUser = true;
+let isGoogleUser = false;
+
+console.log(isEmailUser || isGoogleUser);
+
+
+
