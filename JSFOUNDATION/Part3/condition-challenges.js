3152ff77617b9915a2d7760e3500e022f2ec1884
Srv228 <deysourav756@gmail.com>
2025-07-03 12:28:58 +0530

    Day4

---
+++ JSFOUNDATION/Part3/condition-challenges.js
@@ -0,0 +1,60 @@
+//conditions --- check ---either true or false
+
+//checking if a numebr is greater tgan another number
+
+let num1 = 5;
+let num2 = 8;
+
+// console.log("Upperrcode");
+
+if (num1 > num2) {
+  //   console.log("num1 is greater than num2");
+} else {
+  //   console.log("Nope , num1 is Not greater than num2");
+}
+
+// console.log("Lowercode");6
+
+//checking if a string is rqual to another string
+
+let userName1 = "Chai";
+let userName2 = "Chai";
+
+if (userName1 == userName2) {
+  //   console.log("Pick another username");
+} else {
+  //   console.log("You can pick this username");
+}
+
+//checking if a variable is a numer or not
+
+let score = 44;
+
+if (typeof score === "number") {
+  //   console.log("Yep , this is a number");
+} else {
+  //   console.log("No , that is not a number");
+}
+
+//checking if a boolean value is true or false
+
+let isTeaReddy = false;
+
+if (isTeaReddy) {
+  console.log("Tea is Reddy");
+} else {
+  console.log();
+  console.log("Tea is Not Reddy");
+}
+
+//check if an array is empty or not
+
+let items = ["iteam1"];
+
+console.log(items.length);
+
+if (items.length === 0) {
+  console.log("Array is empty");
+} else {
+  console.log("Array is not empty");
+}
